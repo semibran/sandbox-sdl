83a676b0cc6ab476eabd9dcac2295407c02ecfbe
Brandon Semilla <semibran@gmail.com>
2020-11-25 01:34:38 -0800

    some debugging prep for sdl blurry renders

--- src/main.c
+++ src/main.c
@@ -3,13 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 
-const int scale = 4;
+const int scale = 1;
 const int width = 64 * scale;
 const int height = 64 * scale;
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
 	SDL_Window *window = SDL_CreateWindow("Game",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		width, height, SDL_WINDOW_SHOWN);
@@ -18,6 +17,7 @@ int main() {
 	int flags = IMG_INIT_PNG;
 	int initted = IMG_Init(flags);
 	SDL_Surface* surface = IMG_Load("src/goku.png");
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(rend, surface);
 	SDL_Rect dest = { 0, 0, surface->w, surface->h };
 
